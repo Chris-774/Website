2d196284e6ca5bd081763902f452ff881622a0ff
Chris Brant <chris.brant@live.co.uk>
2017-12-28 20:32:12 +0000

    Battled with Google maps to no avail, added more detial in contact and experience sections

--- javascript.js
+++ javascript.js
@@ -1,25 +1,27 @@
       function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        let uluru = {lat: -25.363, lng: 131.044};
+        let map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
         });
-        var marker = new google.maps.Marker({
+        let marker = new google.maps.Marker({
           position: uluru,
           map: map
         });
       }
 
-window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("nav-bar");
-var sticky = header.offsetTop;
 
-function myFunction() {
-	if (window.pageYOffset >= sticky) {
-		header.classList.add("sticky");
-	} else {
-		header.classList.remove("sticky");
-	}
-}
+//window.onscroll = function() {myFunction()};
+//
+//let header = document.getElementById("nav-bar");
+//let sticky = header.offsetTop;
+//
+//function myFunction() {
+//	if (window.pageYOffset >= sticky) {
+//		header.classList.add("sticky");
+//	} else {
+//		header.classList.remove("sticky");
+//	}
+//}
 
